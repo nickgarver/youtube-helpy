6eb2b57c08251bb8141c8be8a1f733a711211d15
nickgarver <nickgarver5@gmail.com>
2020-12-21 15:39:58 -0500

    fix stuff

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react"
+import { useDropzone } from "react-dropzone"
+import logo from "./logo.svg";
+import "./App.css"
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  const [files, setFiles] = useState([])
+
+  const { getRootProps, getInputProps } = useDropzone({
+    maxFiles: 1, // number of files,
+    accept: "audio/mpeg",
+    onDrop: (acceptedFiles) => {
+      setFiles(
+        acceptedFiles.map((file) =>
+          Object.assign(file, {
+            preview: URL.createObjectURL(file),
+          })
+        )
+      )
+    },
+  })
+
+  const images = files.map((file) => (
+    <div key={file.name}>
+      <div>
+        <audio controls>
+          <source src={file.preview} type="audio/mpeg"/>
+        </audio>
+      </div>
     </div>
-  );
+  ))
+
+  return (
+    <div className="App min-h-screen text-blue-200 flex items-center flex-col p-20">
+
+      <div className="mb-10 grid grid-cols-4 grid-rows-2 w-1/2 mx-auto">
+        <img className="opacity-25" src={logo} alt="React Logo" width="300" />
+          <div className="col-span-2 row-span-3" {...getRootProps()}>
+            <input {...getInputProps()} />
+            <p className="text-2xl lg:text-3xl mb-10 text-center">Drop mp3 here</p>
+          </div>
+        <img className="opacity-25" src={logo} alt="React Logo" width="300" />
+        <img className="opacity-25" src={logo} alt="React Logo" width="300" />
+        <img className="opacity-25" src={logo} alt="React Logo" width="300" />
+        <div>{images}</div>
+      </div>
+    </div>
+  )
 }
 
-export default App;
+export default App
