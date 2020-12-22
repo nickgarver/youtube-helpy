4ad67dd8b5fe7dff8580f3b789a4a9a7683d25d9
nickgarver <nickgarver5@gmail.com>
2020-12-22 13:44:11 -0500

    upload works, fixed audio player

--- client/src/App.js
+++ client/src/App.js
@@ -1,67 +1,13 @@
 import React from 'react';
 import FileUpload from './components/FileUpload';
 import './App.css';
+import logo from "./logo.svg";
 
 const App = () => (
-  <div className='container mt-4'>
-    <h4 className='display-4 text-center mb-4'>
-      <i className='fab fa-react' /> React File Upload
-    </h4>
-
-    <FileUpload />
+  <div className="App min-h-screen text-blue-200 flex items-center flex-col p-20">
+  <FileUpload />
+      <img className="opacity-25" src={logo} alt="React Logo" width="30" />
   </div>
 );
 
 export default App;
-
-// import React, { useState } from "react"
-// import { useDropzone } from "react-dropzone"
-// import logo from "./logo.svg";
-// import "./App.css"
-//
-// function App() {
-//   const [files, setFiles] = useState([])
-//
-//   const { getRootProps, getInputProps } = useDropzone({
-//     maxFiles: 1, // number of files,
-//     accept: "audio/mpeg",
-//     onDrop: (acceptedFiles) => {
-//       setFiles(
-//         acceptedFiles.map((file) =>
-//           Object.assign(file, {
-//             preview: URL.createObjectURL(file),
-//           })
-//         )
-//       )
-//     },
-//   })
-//
-//   const images = files.map((file) => (
-//     <div key={file.name}>
-//       <div>
-//         <audio controls>
-//           <source src={file.preview} type="audio/mpeg"/>
-//         </audio>
-//       </div>
-//     </div>
-//   ))
-//
-//   return (
-//     <div className="App min-h-screen text-blue-200 flex items-center flex-col p-20">
-//
-//       <div className="mb-10 grid grid-cols-4 grid-rows-2 w-1/2 mx-auto">
-//         <img className="opacity-25" src={logo} alt="React Logo" width="300" />
-//           <div className="col-span-2 row-span-3" {...getRootProps()}>
-//             <input {...getInputProps()} />
-//             <p className="text-2xl lg:text-3xl mb-10 text-center">Drop mp3 here</p>
-//           </div>
-//         <img className="opacity-25" src={logo} alt="React Logo" width="300" />
-//         <img className="opacity-25" src={logo} alt="React Logo" width="300" />
-//         <img className="opacity-25" src={logo} alt="React Logo" width="300" />
-//         <div>{images}</div>
-//       </div>
-//     </div>
-//   )
-// }
-//
-// export default App
--- client/src/components/FileUpload.js
+++ client/src/components/FileUpload.js
@@ -1,19 +1,35 @@
 import React, { Fragment, useState } from 'react';
 import Message from './Message';
 import Progress from './Progress';
+import { useDropzone } from "react-dropzone"
 import axios from 'axios';
+import ReactAudioPlayer from 'react-audio-player';
 
 const FileUpload = () => {
-  const [file, setFile] = useState('');
-  const [filename, setFilename] = useState('Choose File');
+  const [file, setFile] = useState({});
+  const [filename, setFilename] = useState('Drop Mp3 here');
   const [uploadedFile, setUploadedFile] = useState({});
   const [message, setMessage] = useState('');
   const [uploadPercentage, setUploadPercentage] = useState(0);
 
-  const onChange = e => {
-    setFile(e.target.files[0]);
-    setFilename(e.target.files[0].name);
-  };
+  const { getRootProps, getInputProps } = useDropzone({
+    maxFiles: 1, // number of files,
+    accept: "audio/mpeg",
+    onDrop: (acceptedFile) => {
+      setFile(
+        // convert preview string into a URL
+        Object.assign(acceptedFile[0], {
+          preview: URL.createObjectURL(acceptedFile[0]),
+        })
+      );
+      setFilename(acceptedFile[0].name);
+    },
+  })
+
+  // const onChange = acceptedFiles => {
+  //   setFile(acceptedFiles.target.files[0]);
+  //   setFilename(acceptedFiles.target.files[0].name);
+  // };
 
   const onSubmit = async e => {
     e.preventDefault();
@@ -46,6 +62,7 @@ const FileUpload = () => {
       if (err.response.status === 500) {
         setMessage('There was a problem with the server');
       } else {
+        //ffmpeg then youtube here
         setMessage(err.response.data.msg);
       }
     }
@@ -55,13 +72,8 @@ const FileUpload = () => {
     <Fragment>
       {message ? <Message msg={message} /> : null}
       <form onSubmit={onSubmit}>
-        <div className='custom-file mb-4'>
-          <input
-            type='file'
-            className='custom-file-input'
-            id='customFile'
-            onChange={onChange}
-          />
+        <div className='custom-file mb-4' {...getRootProps()}>
+          <input id='customFile' {...getInputProps()} />
           <label className='custom-file-label' htmlFor='customFile'>
             {filename}
           </label>
@@ -75,11 +87,12 @@ const FileUpload = () => {
           className='btn btn-primary btn-block mt-4'
         />
       </form>
+
       {uploadedFile ? (
         <div className='row mt-5'>
           <div className='col-md-6 m-auto'>
             <h3 className='text-center'>{uploadedFile.fileName}</h3>
-            <img style={{ width: '100%' }} src={uploadedFile.filePath} alt='' />
+            <ReactAudioPlayer src={file.preview} controls/>
           </div>
         </div>
       ) : null}
